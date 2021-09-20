9db3a3ceb6d89887fa1c6cc8eb025600bf44f749
adailsonaguiar <adailsonacj@live.com>
2021-09-20 10:59:34 -0300

    analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import Analytics from "../components/Analytics";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -37,6 +38,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );
