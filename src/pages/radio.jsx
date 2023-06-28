f883f652f82e6e0bc9ba72c64d99269857bdc02b
adailsonaguiar <adailsonacj@live.com>
2023-06-28 15:33:40 -0300

    fix

--- src/pages/radio.jsx
+++ src/pages/radio.jsx
@@ -7,7 +7,7 @@ export default function Radio() {
     <BodyTemplate>
       <div className="flex flex-col w-full">
         <div className="flex justify-center items-center w-full p-10">
-          <div className="flex bg-white rounded-xl w-full md:w-3/12">
+          <div className="flex bg-white rounded-xl w-full md:w-3/12 z-10">
             <img
               className="w-full object-contain"
               src="https://radiopazpalmas.com.br/wp-content/uploads/2022/07/Logo-Radio-Paz.png"
